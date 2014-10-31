0e44e4a9e65945db4dffb2ea08dd13471e56ec59
pedrohpp <pedro.henriquehpp@gmail.com>
2014-10-31 14:25:05 -0200

    empresa : Dream_and_Play
    uyfyrf

---
+++ empresa.py
@@ -0,0 +1,24 @@
+#programa :P calculadora
+def soma ():
+    pass
+def multipli():
+    pass
+def divi():
+    pass
+def sub():
+    pass
+#--------programa principal------------
+def programa_principal():
+    x=input("insira o valor #1: ")
+    y= input("insira o valor #2: ")
+    operação = ("qual operação?")
+    if soma ('+'):
+        soma()
+    if multipli ('*'):
+        multipli()
+    if divi('/'):
+        divi()
+    if sub('-'):
+        sub ()
+#chamando a função   
+programa_principal()
